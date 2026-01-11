d980385b9296530011c353b5325c113d57c86ddf
Anton Akhmerov <anton.akhmerov@gmail.com>
2026-01-11 22:26:03 +0100

    add MPI builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,10 @@ set -ex
 
 mkdir builddir
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" && "${mpi:-}" == "openmpi" ]]; then
+  export OPAL_PREFIX="${PREFIX}"
+fi
+
 if [ -n "${SP_DIR:-}" ]; then
   if [ -n "${PKG_CONFIG_PATH:-}" ]; then
     export PKG_CONFIG_PATH="${SP_DIR}/numpy/_core/lib/pkgconfig:${PKG_CONFIG_PATH}"
